2a6114f3421fa9a7f7a70fc8d2be0f8f84578364
UnathiNgutyana <unathingutyana@gmail.com>
2024-02-17 09:42:28 +0200

    Practice update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,4 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 
-// Press Shift+F9 to start debugging your code. We have set one breakpoint
-// for you, but you can always add more by pressing Ctrl+F8.
-
-// Press Shift+F10 or click the green arrow button in the gutter to run the code.
-
-// Press Alt+Enter with your caret at the highlighted text to see how
-// IntelliJ IDEA suggests fixing it.
 
 import java.time.LocalDate;
 
