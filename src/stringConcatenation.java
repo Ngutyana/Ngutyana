8e142649fad3176a4ddbd3db97a1e113771dea97
UnathiNgutyana <unathingutyana@gmail.com>
2024-02-19 00:15:17 +0200

    Java basics practice

--- src/stringConcatenation.java
+++ src/stringConcatenation.java
@@ -1,12 +1,16 @@
 public class stringConcatenation
 {
-    //No 1
     public static void main(String[] args) {
-        String text1 = "My first text ";
+        String text1 = "My first text";
         String text2 = "My second text";
+
+        //No 1
         System.out.println(text1 + text2);
 
         //No 2
         System.out.println(text1 +" "+ text2); //adding space
+
+        //No 3
+        System.out.println(text1.concat(text2)); //adding concat
     }
 }
